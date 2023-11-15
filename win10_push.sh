9526a84b693b3706f3d678b85f85404ab449020d
cancerai <cancerai@163.com>
2023-11-15 10:17:23 +0800

    file

--- win10_push.sh
+++ win10_push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git config --global user.name "cancerai"
 git config --global user.email "cancerai@163.com"
-git remote rm *
+git remote rm origin
 git remote add origin https://gitee.com/cancerai/demo
 git add .
 git commit -m "file"
