599d56c202eb661542a44be9d7976f425d59de63
cancerai <cancerai@163.com>
2023-11-23 09:15:18 +0800

    file

--- C/11-23/7.c
+++ C/11-23/7.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main () {
-	int i;
+	int i,sum;
 	for (i=1;i<100;i++) {
 		if(i % 3 == 0) {
-			printf("%d\n", i);
+			sum += i;
 		}
 	}
+			printf("1~100以内，能被3整除的数的和: %d\n", sum);
 }
