cf1cb711301f4f4f0fb1d1c1bdf946d97aa0b7a9
cancerai <cancerai@163.com>
2023-11-23 09:21:06 +0800

    file

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-git init
-git config --global user.name "cancerai"
-git config --global user.email "cancerai@163.com"
-git remote rm origin
-git remote add origin https://gitee.com/cancerai/demo
-git add .
-git commit -m "file"
-git push origin master:master
+git init >> log
+git config --global user.name "cancerai"  >> log
+git config --global user.email "cancerai@163.com"  >> log
+git remote rm origin  >> log
+git remote add origin https://gitee.com/cancerai/demo  >> log
+git add .  >> log
+git commit -m "file"  >> log
+git push origin master:master  >> log
