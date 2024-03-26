e6e07458d172f6bf31989acb9cf2d626894ecae2
CancerAi <cancerai@163.com>
2024-03-26 01:36:45 +0000

    update comment
    Signed-off-by: CancerAi <cancerai@163.com>

--- 二(下)/mysql/03-26/sql.sql
+++ 二(下)/mysql/03-26/sql.sql
@@ -1,23 +1,23 @@
-# delete database when it not exist.
+-- delete database when it not exist.
 drop database school if exist;
 
-# create new database
+-- create new database
 create database school;
 
-# entry database
+-- entry database
 use school;
 
-# create new table
+-- create new table
 create table user(
     number char(10) not null,
     name char(10) not null
 );
 
-# delete the table
+-- delete the table
 drop table user;
 
-# delete database
+-- delete database
 drop database school;
 
 
-# DONE
+-- DONE
