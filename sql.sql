8b27bde2ca0f8d01899e3ef8021f2764319373ad
CancerAi <cancerai@163.com>
2024-11-06 09:18:27 +0800

    sd

--- sql.sql
+++ sql.sql
@@ -5,12 +5,12 @@ SELECT 姓名 as 'name', YEAR(GETDATE()) - YEAR([出生日期]) as age from stud
 
 -- 1(2)
 SELECT 
-CASE 成绩
-	WHEN 成绩 > 90 THEN '优',
-  when 成绩 >= 75 THEN '良',
-  when 成绩 >= 60 THEN '及格',
-  when 成绩 >= 60 THEN '不及格',
-END
+CASE 
+	WHEN 成绩 > 90 THEN '优' 
+  when 成绩 BETWEEN 75 and 90 THEN '良' 
+  when 成绩 BETWEEN 60 and 75 THEN '及格'
+  when 成绩 < 60 THEN '不及格'
+END as '成绩'
 from score
 
 -- 1(3)
@@ -26,4 +26,10 @@ SELECT * from student where [姓名] like '王__'
 select student.[学号], student.[姓名], course.[课程号], score.[成绩] from student, score , course
 
 -- 2(2)
-SELECT * FROM 
+SELECT student.[学号], student.[姓名], class.[班级名称] FROM student inner join class on student.[学号] = (SELECT * from) where class.[院系] = '会计学院'
+
+-- 2(3)
+SELECT student.[学号], student.[姓名], score.[成绩] from student join score on student.[学号] = score.[学号] where score.[成绩] >= 90
+
+-- 2(4)
+SELECT 
