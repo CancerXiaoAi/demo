7c764117343bdc5b9bd58a071f8862db5f84c09a
CancerAi <cancerai@163.com>
2024-11-06 08:57:23 +0800

    sd

--- sql.sql
+++ sql.sql
@@ -1,9 +1,9 @@
 use [xujg-schooldb]
 
--- 1
+-- 1(1)
 SELECT 姓名 as 'name', YEAR(GETDATE()) - YEAR([出生日期]) as age from student
 
--- 2
+-- 1(2)
 SELECT 
 CASE 成绩
 	WHEN 成绩 > 90 THEN '优',
@@ -13,5 +13,17 @@ CASE 成绩
 END
 from score
 
--- 3
-select [课程名], [学分] from course where 
+-- 1(3)
+select [课程名], [学分] from course where [学时] >= 48
+
+-- 1(4)
+select [课程名], [学期] from course where [前置课] is null
+
+-- 1(5)
+SELECT * from student where [姓名] like '王__'
+
+-- 2(1)
+select student.[学号], student.[姓名], course.[课程号], score.[成绩] from student, score , course
+
+-- 2(2)
+SELECT * FROM 
