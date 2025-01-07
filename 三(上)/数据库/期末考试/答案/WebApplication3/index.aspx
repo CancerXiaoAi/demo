733541b5a8c56422da8ff9bce0de1213f59f50e7
CancerAi <cancerai@163.com>
2025-01-08 07:00:08 +0800

    add: 答案folder

---
+++ 三(上)/数据库/期末考试/答案/WebApplication3/index.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WebApplication3.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style>
+        td{
+            border: 1px solid #000;
+        }
+    </style>
+</head>
+<body>
+    <form runat="server">
+        <asp:Table runat="server">
+            <asp:TableRow>
+                <asp:TableCell>香蕉</asp:TableCell>
+                <asp:TableCell>10元</asp:TableCell>
+                <asp:TableCell><asp:Button id="banana" Text="+" runat="server" OnClick="add"/></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>苹果</asp:TableCell>
+                <asp:TableCell>5元</asp:TableCell>
+                <asp:TableCell><asp:Button id="apple" Text="+" runat="server" OnClick="add"/></asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>梨</asp:TableCell>
+                <asp:TableCell>5元</asp:TableCell>
+                <asp:TableCell><asp:Button id="pair" Text="+" runat="server" OnClick="add"/></asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <asp:Button runat="server" OnClick="submit" Text="结算"/>
+        <asp:Button runat="server" OnClick="clean" Text="清零"/>
+    </form>
+</body>
+</html>
---
+++ 三(上)/数据库/期末考试/答案/WebApplication3/index.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class index : System.Web.UI.Page
+    {
+        static int bananaSum = 0;
+        static int appleSum = 0;
+        static int pairSum = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.ID == "banana")
+            {
+                bananaSum++;
+            }
+
+                if (button.ID == "apple")
+                {
+                    appleSum++;
+                }
+
+            if (button.ID == "pair")
+            {
+                pairSum++;
+            }
+
+            Response.Write("香蕉买了 " + bananaSum + " 个<br>");
+            Response.Write("苹果买了 " + appleSum + " 个<br>");
+            Response.Write("梨买了 " + pairSum + " 个<br>");
+        }
+
+        protected void submit(object sender, EventArgs e)
+        {
+            Dictionary<String, int> fruit = new Dictionary<string, int>();
+            int bananaPrice = bananaSum * 10;
+            int applePrice = appleSum * 5;
+            int pairPrice = pairSum * 5;
+
+            fruit.Add("banana", bananaPrice);
+            fruit.Add("apple", applePrice);
+            fruit.Add("pair", pairPrice);
+            // craete Table first
+            createTabel();
+            foreach (var item in fruit)
+            {
+                String key = item.Key;
+                int value = item.Value;
+                saveToDB(key, value);
+            }
+        }
+
+        private void createTabel()
+        {
+            String tableName = "test";
+            String connStr = "Data Source=10.145.152.185;Database=xujg-schooldb;User Id=user-xujg;Password=xu-2231;Encrypt=false";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            // create a table
+            SqlCommand createTable = new SqlCommand("create table " + tableName + "(" +
+                "id varchar(30)," +
+                "name varchar(20)," +
+                "price int" +
+                ")", conn);
+            createTable.ExecuteNonQuery();
+        }
+        private void saveToDB(String name, int price)
+        {
+            // define some database connection information
+            String tableName = "test";
+            String connStr = "Data Source=10.145.152.185;Database=xujg-schooldb;User Id=user-xujg;Password=xu-2231;Encrypt=false";
+            SqlConnection conn= new SqlConnection(connStr);
+            conn.Open();
+
+            // save to database
+            String now = DateTime.Now.ToString();
+            SqlCommand saveData = new SqlCommand("insert into " + tableName + " values(\'"+now+"\', \'"+name+"\', "+price+")", conn);
+            saveData.ExecuteNonQuery();
+        }
+
+        protected void clean(object sender, EventArgs e)
+        {
+            bananaSum = 0;
+            appleSum = 0;
+            pairSum = 0;
+            Response.Write("香蕉买了 " + bananaSum + " 个<br>");
+            Response.Write("苹果买了 " + appleSum + " 个<br>");
+            Response.Write("梨买了 " + pairSum + " 个<br>");
+        }
+    }
+}
